5c68bd4ed4c047211d258d49d7285b17b88a3438
Mengye Ren <renmengye@gmail.com>
2023-04-11 17:50:37 -0400

    custom css and figure sizing

--- src/converter/postprocessor/figures.js
+++ src/converter/postprocessor/figures.js
@@ -7,5 +7,22 @@ module.exports = function (document) {
     ) {
       figure.appendChild(figure.children[0]);
     }
+    // Do not reduce subfigure.
+    if (figure.children[0].tagName != "TABLE"){
+      // Resize images to bump up resolution.
+      // We assume that latexML will always have width
+      Array.from(figure.getElementsByTagName("img")).forEach((img) => {
+        width = img.getAttribute("width");
+        height = img.getAttribute("height");
+        img.setAttribute("width", Math.floor(parseInt(width) / 4.0));
+        img.setAttribute("height", Math.floor(parseInt(height) / 4.0));
+      });
+    }
+    else{
+      // no margin for subfigure
+      Array.from(figure.getElementsByTagName("figure")).forEach((fig) => {
+        fig.style.margin = "0px";
+      });
+    }
   });
 };
